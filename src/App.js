a6812c0c878209774e4dab4ef34543ef727f8489
Crs <cristian.dias@terraviva.agr.br>
2022-02-28 22:15:51 -0300

    feat: Criação componentes, arquivo 'App.js'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import NoteList from './components/NoteList'
 
 function App() {
   return (
-    <h1>teste</h1>
+    /*
+      form - Criação componente formulario
+      */
+    <section>
+      <form>
+        <h1>App React</h1>
+        <input type='text' placeholder='Título'></input>
+        <textarea placeholder='Escreva sua nota...' />
+        <button> Criar Nota</button>
+
+       
+      </form>
+      <NoteList/>
+    </section>
+
   );
 }
 
