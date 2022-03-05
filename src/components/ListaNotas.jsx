c51bf4ef3ebaef8aed8d30662baaf471100875dc
Crs <cristian.dias@terraviva.agr.br>
2022-03-05 11:13:26 -0300

    feat: Personalização retorno de conteúdo via lista, arquivo 'ListaNotas.jsx'

--- src/components/ListaNotas.jsx
+++ src/components/ListaNotas.jsx
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
 import CardNota from "./CardNota";
 
+/*
+    {} - Entende como comando JS
+*/
 class ListaNotas extends Component {
   render() {
     return (
       <ul>
-        <li>
-          <CardNota />
-        </li><li>
-          <CardNota />
-        </li><li>
-          <CardNota />
-        </li>
+        {
+          /*
+          Ao criar objeto Array, com a função 'Array' e popular com o '.of()', é possível 
+          executar uma função a partir de cada item do Array, utilizando o '.map(nomeFunção)'
+          */
+          Array.of('Trabalho', 'Trabalho', 'Estudos').map((categoria) => {
+            return (
+              /*Para entender o recebimento de valores via variável, 
+              é necessário passar entre '{}'*/
+              <li>
+                <div>{categoria} </div>
+                <CardNota />
+              </li>
+            );
+          })
+        }
       </ul>
     );
   }
