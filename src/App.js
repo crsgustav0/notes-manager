ed2a9e451ddd7a9101a95dc387e2f8378a23e1f4
Crs <cristian.dias@terraviva.agr.br>
2022-03-07 20:26:28 -0300

    feat: Criação função 'criarNota(), arquivo 'App.js'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 
 import FormularioCadastro from './components/FormularioCadastro';
-import ListaNotas from './components/ListaNotas'; 
+import ListaNotas from './components/ListaNotas';
 import "./assets/App.css";
 import "./assets/index.css";
 
 class App extends Component {
+
+  criarNota(titulo, texto) {
+    console.log(`Nota ${titulo} ${texto}`)
+  }
+
   render() {
     return (
       <section className='conteudo'>
-        <FormularioCadastro />
+        <FormularioCadastro
+          criarNota={this.criarNota}
+        />
         <ListaNotas />
       </section>
     );
