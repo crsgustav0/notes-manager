e3644bc347dfdc6ffd8b271272b56ef1b2453abb
Crs <cristian.dias@terraviva.agr.br>
2022-03-07 21:16:08 -0300

    feat: Definição de renderização e estado via criação de objetos, arquivo 'App.js'

--- src/App.js
+++ src/App.js
@@ -9,27 +9,36 @@ class App extends Component {
 
   constructor() {
     super()
-    this.notas = [];
+    // state - Propriedade 'React' que ao ser executada chama automaticamente a função 'render()'
+    this.state = {
+      notas: []
+    };
   }
 
   criarNota(titulo, texto) {
     // Criação de novo objeto
     const novaNota = { titulo, texto }
-    // Inclusão do objeto 'novaNota' ao array 'notas' via função 'push()'
-    this.notas.push(novaNota);
-    console.log(novaNota)
+    //Recebe todo o conteudo e atribui a uma variável 
+    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas: novoArrayNotas
+    }
+    /*
+    State: Serve para guardar valores/estado que podem vir a mudar com a interação do usuário com o componente,
+    */
+    this.setState(novoEstado)
   }
 
   render() {
     return (
       /*
       bind -  Associa o comportamento do componente a instância do método criada em seu módulo
-      */ 
+      */
       <section className='conteudo'>
         <FormularioCadastro
           criarNota={this.criarNota.bind(this)}
         />
-        <ListaNotas notas={this.notas}
+        <ListaNotas notas={this.state.notas}
         />
       </section>
     );
