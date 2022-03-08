891023db844fba3cae4ee7d01420b7f5a1f73334
Crs <cristian.dias@terraviva.agr.br>
2022-03-07 21:15:28 -0300

    fix: Acesso a métodos classes via propriedade 'props', arquivo 'CardNota.jsx'

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -5,12 +5,20 @@ class CardNota extends Component {
     render() {
         return (
             /*Para fazer a estilização dos arquivos via CSS
-            é necessário importar a classe via função 'className' */
+            é necessário importar a classe via função 'className' 
+            Props: Valores para a configuração de um componente, essas props são passadas pelo elemento acima 
+            (que utiliza o componente que irá receber) e são imutáveis, 
+            utilizado para a comunicação de componentes
+            */
             <section className="card-nota">
                 <header className='card-nota-cabecalho'>
-                    <h3 className='card-nota_titulo'>Título</h3>
+                    <h3 className='card-nota_titulo'>{
+                        this.props.titulo
+                    }</h3>
                 </header>
-                <p className='card-nota_texto'>Escreva sua nota</p>
+                <p className='card-nota_texto'>{
+                    this.props.texto
+                }</p>
             </section>
         );
     }
