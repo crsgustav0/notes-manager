a76a313702eccff5ca00e6fed0d8151cf28a28d0
Crs <cristian.dias@terraviva.agr.br>
2022-05-17 19:22:04 -0300

    feat: Inclusão estilização via CSS, arquivos 'ListaCategorias.jsx' e 'estilo.css'

--- src/components/ListaCategorias/ListaCategorias.jsx
+++ src/components/ListaCategorias/ListaCategorias.jsx
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-
+import './estilo.css'
 class ListaCategorias extends Component {
 
     _handleEventoInput(e) {
         if (e.key == 'Enter') {
-            console.log('Adicionar Categoria')
+            let valorCategoria = e.target.value;
+            this.props.adicionarCategoria(valorCategoria)
         }
-    } 
+    }
 
     render() {
         return (
-            <section>
-                <ul>
-                    <li>Categorias</li>
-                    <li>Categorias</li>
-                    <li>Categorias</li>
-                    <li>Categorias</li>
-                    <li>Categorias</li>
+            <section className='lista-categorias'>
+                <ul className='lista-categorias_lista'>
+                    {this.props.categorias.map((categoria, index) => {
+                        return <li
+                            key={index}
+                            className='lista-categorias_item'>
+                            {categoria}
+                        </li>
+                    })}
                 </ul>
                 <input
                     type="text"
+                    className='lista-categorias_input'
                     placeholder='Adicionar categoria'
                     onKeyUp={this._handleEventoInput.bind(this)}
                 />
